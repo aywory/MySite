4ad1ff98aae3c668ef5e06e502b461bc24b1f06c
aywory <153110264+aywory@users.noreply.github.com>
2026-05-05 19:07:38 +0300

    исправление обводки

--- assets/js/home-title-animation.js
+++ assets/js/home-title-animation.js
@@ -15,9 +15,11 @@
     if (symbol === " ") {
       symbolNode.className = "hero-char hero-space";
       symbolNode.textContent = "\u00A0";
+      symbolNode.dataset.char = "\u00A0";
     } else {
       symbolNode.className = "hero-char";
       symbolNode.textContent = symbol;
+      symbolNode.dataset.char = symbol;
     }
 
     symbolNode.style.setProperty("--hero-char-index", String(index));
